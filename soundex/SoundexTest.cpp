2aa5d0ea20d6b78dd7e8b1ef5007bb67f639ce4d
Krishnakumar T M <krishnakumar.tm@gmail.com>
2015-11-18 22:11:04 +0530

    Finished soundex rule 2

--- soundex/SoundexTest.cpp
+++ soundex/SoundexTest.cpp
@@ -1,24 +1,48 @@
 #include <gmock\gmock.h>
+#include "Soundex.h"
 
-class Soundex
+using testing::Eq;
+
+class SoundexEncoding : public testing::Test
 {
 public:
-	Soundex();
-	~Soundex();
+	SoundexEncoding();
+	~SoundexEncoding();
 
 private:
-
+public:
+	Soundex oSoundex;
 };
 
-Soundex::Soundex()
+SoundexEncoding::SoundexEncoding()
 {
 }
 
-Soundex::~Soundex()
+SoundexEncoding::~SoundexEncoding()
 {
 }
 
-TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord)
+TEST_F(SoundexEncoding, RetainSoleLetterOfOneLetterWord)
 {
-	Soundex oSoundex;
+	ASSERT_THAT(oSoundex.encode(L"A"), Eq(L"A000"));
+}
+
+TEST_F(SoundexEncoding, PadWithZerosToEnsureThreeDigits)
+{
+	ASSERT_THAT(oSoundex.encode(L"I"), Eq(L"I000"));
+}
+
+TEST_F(SoundexEncoding, ReplaceConsonantsWithAppropriateDigit)
+{
+	ASSERT_THAT(oSoundex.encode(L"Ax"), Eq(L"A200"));
+}
+
+TEST_F(SoundexEncoding, IgnoreNonAlphabets)
+{
+	ASSERT_THAT(oSoundex.encode(L"A#"), Eq(L"A000"));
+}
+
+TEST_F(SoundexEncoding, ReplaceMultipleConsonentsWithDigits)
+{
+	ASSERT_THAT(oSoundex.encode(L"Acdl"), Eq(L"A234"));
 }
